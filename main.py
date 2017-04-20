a8be56d7cc1a5e92db67850eb1845dc12233134b
powerboat9 <powerboat9@users.noreply.github.com>
2017-04-20 14:40:18 -0400

    Started work

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
 #!/usr/bin/python
+import http.server.*
+
+def listRokus():
+    req = HTTPConnection("239.255.255.250", 1900)
+    req.request("M-SEARCH", "*", headers = {"Host": "239.255.255.250:1900", "Man": "ssdp:discover", "ST": "roku:ecp"})
+    req.
